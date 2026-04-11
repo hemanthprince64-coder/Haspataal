d160cd0e68e16ea5f3417e1c974914a1cbb853f8
Haspataal Dev <haspataal@dev.com>
2026-04-12 00:23:12 +0530

    fix(config): add supabase and unsplash to remotePatterns for next/image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'deqcehtgwbpmwlajsore.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
   async headers() {
     return [
       {
