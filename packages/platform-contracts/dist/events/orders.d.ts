7f562fec94ff13c2e7cb2e9aeb44d3fb8f3b5894
Haspataal Dev <haspataal@dev.com>
2026-07-24 18:44:02 +0530

    chore(infra): update dist artifacts across packages

--- packages/platform-contracts/dist/events/orders.d.ts
+++ packages/platform-contracts/dist/events/orders.d.ts
@@ -1,82 +1,119 @@
 import { z } from 'zod';
+
 export declare const ORDER_EVENTS: {
-    readonly ORDER_REQUESTED: "ORDER_REQUESTED";
-    readonly ORDER_AMENDED: "ORDER_AMENDED";
-    readonly ORDER_CANCELLED: "ORDER_CANCELLED";
-    readonly ORDER_COMPLETED: "ORDER_COMPLETED";
-    readonly ORDER_PRIORITY_CHANGED: "ORDER_PRIORITY_CHANGED";
-    readonly ORDER_EXECUTION_UPDATED: "ORDER_EXECUTION_UPDATED";
+  readonly ORDER_REQUESTED: 'ORDER_REQUESTED';
+  readonly ORDER_AMENDED: 'ORDER_AMENDED';
+  readonly ORDER_CANCELLED: 'ORDER_CANCELLED';
+  readonly ORDER_COMPLETED: 'ORDER_COMPLETED';
+  readonly ORDER_PRIORITY_CHANGED: 'ORDER_PRIORITY_CHANGED';
+  readonly ORDER_EXECUTION_UPDATED: 'ORDER_EXECUTION_UPDATED';
 };
-export declare const OrderRequestedPayloadSchema: z.ZodObject<{
+export declare const OrderRequestedPayloadSchema: z.ZodObject<
+  {
     orderId: z.ZodString;
-    items: z.ZodArray<z.ZodString, "many">;
-}, "strip", z.ZodTypeAny, {
+    items: z.ZodArray<z.ZodString, 'many'>;
+  },
+  'strip',
+  z.ZodTypeAny,
+  {
     orderId: string;
     items: string[];
-}, {
+  },
+  {
     orderId: string;
     items: string[];
-}>;
+  }
+>;
 export type OrderRequestedPayload = z.infer<typeof OrderRequestedPayloadSchema>;
-export declare const OrderAmendedPayloadSchema: z.ZodObject<{
+export declare const OrderAmendedPayloadSchema: z.ZodObject<
+  {
     orderId: z.ZodString;
     version: z.ZodNumber;
-    items: z.ZodArray<z.ZodString, "many">;
-}, "strip", z.ZodTypeAny, {
+    items: z.ZodArray<z.ZodString, 'many'>;
+  },
+  'strip',
+  z.ZodTypeAny,
+  {
     orderId: string;
     items: string[];
     version: number;
-}, {
+  },
+  {
     orderId: string;
     items: string[];
     version: number;
-}>;
+  }
+>;
 export type OrderAmendedPayload = z.infer<typeof OrderAmendedPayloadSchema>;
-export declare const OrderCancelledPayloadSchema: z.ZodObject<{
+export declare const OrderCancelledPayloadSchema: z.ZodObject<
+  {
     orderId: z.ZodString;
     reason: z.ZodString;
-}, "strip", z.ZodTypeAny, {
+  },
+  'strip',
+  z.ZodTypeAny,
+  {
     orderId: string;
     reason: string;
-}, {
+  },
+  {
     orderId: string;
     reason: string;
-}>;
+  }
+>;
 export type OrderCancelledPayload = z.infer<typeof OrderCancelledPayloadSchema>;
-export declare const OrderCompletedPayloadSchema: z.ZodObject<{
+export declare const OrderCompletedPayloadSchema: z.ZodObject<
+  {
     orderId: z.ZodString;
-}, "strip", z.ZodTypeAny, {
+  },
+  'strip',
+  z.ZodTypeAny,
+  {
     orderId: string;
-}, {
+  },
+  {
     orderId: string;
-}>;
+  }
+>;
 export type OrderCompletedPayload = z.infer<typeof OrderCompletedPayloadSchema>;
-export declare const OrderPriorityChangedPayloadSchema: z.ZodObject<{
+export declare const OrderPriorityChangedPayloadSchema: z.ZodObject<
+  {
     orderId: z.ZodString;
     priority: z.ZodString;
-}, "strip", z.ZodTypeAny, {
+  },
+  'strip',
+  z.ZodTypeAny,
+  {
     orderId: string;
     priority: string;
-}, {
+  },
+  {
     orderId: string;
     priority: string;
-}>;
+  }
+>;
 export type OrderPriorityChangedPayload = z.infer<typeof OrderPriorityChangedPayloadSchema>;
-export declare const OrderExecutionUpdatedPayloadSchema: z.ZodObject<{
+export declare const OrderExecutionUpdatedPayloadSchema: z.ZodObject<
+  {
     orderId: z.ZodString;
     itemId: z.ZodOptional<z.ZodString>;
     executionId: z.ZodString;
     status: z.ZodString;
-}, "strip", z.ZodTypeAny, {
-    orderId: string;
+  },
+  'strip',
+  z.ZodTypeAny,
+  {
     status: string;
+    orderId: string;
     executionId: string;
     itemId?: string | undefined;
-}, {
-    orderId: string;
+  },
+  {
     status: string;
+    orderId: string;
     executionId: string;
     itemId?: string | undefined;
-}>;
+  }
+>;
 export type OrderExecutionUpdatedPayload = z.infer<typeof OrderExecutionUpdatedPayloadSchema>;
 //# sourceMappingURL=orders.d.ts.map
